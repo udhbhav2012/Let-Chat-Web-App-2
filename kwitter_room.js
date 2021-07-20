267abcb483730d0bbcbb924dfa9da68d5b2aaee4
udhbhav2012 <82303525+udhbhav2012@users.noreply.github.com>
2021-07-20 14:14:12 +0530

    Add files via upload

---
+++ kwitter_room.js
@@ -0,0 +1,13 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyBGUm6CZaU7mgJzRbRFg_FBLsCFX6YaGqY",
+    authDomain: "let-chat-web-app-2.firebaseapp.com",
+    databaseURL: "https://let-chat-web-app-2-default-rtdb.firebaseio.com",
+    projectId: "let-chat-web-app-2",
+    storageBucket: "let-chat-web-app-2.appspot.com",
+    messagingSenderId: "682989632053",
+    appId: "1:682989632053:web:0c2372b83653d669b98f7c",
+    measurementId: "G-F2S53Q3WSP"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  firebase.analytics();
